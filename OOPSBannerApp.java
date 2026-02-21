39928223594c2443683a99b59a04744c94ef5efc
Yashwanth <kjyashwanth586@gmail.com>
2026-02-21 21:24:22 +0530

    Coded UC1 to Print OOPS to Console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,18 @@
+/**
+
+ * 
+ * @author K J YASHWANTH
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * Main method - Entry point of the Java application.
+     * Prints "OOPS" to the console and exits.
+     * 
+     * @param args command-line arguments (not used)
+     */
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
